41c32937bbc41fe85458f1d57b585e36116d972e
Luke_Payne <l.a.n.q.payne@gmail.com>
2026-03-24 04:48:41 +0000

    H: completed C3L3: Updated main.py: Moved player.draw(screen) to after screen.fill(black) in game loop

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def main():
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
-        player.draw(screen)
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 

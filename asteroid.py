059c3aa676e3d7761924ea4f56a2a1d7fc8fa4fb
Luke_Payne <l.a.n.q.payne@gmail.com>
2026-03-25 05:17:46 +0000

    P: Completed C4L5: Updated asteroid.py, main.py: Asteroids now split when shot. Project complete

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
+import random
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
 
@@ -13,3 +15,17 @@ class Asteroid(CircleShape):
     def update(self, dt):
         move = self.velocity * dt
         self.position += move
+
+    def split(self):
+        self.kill()
+        if self.radius > ASTEROID_MIN_RADIUS:
+            log_event("asteroid_split")            
+            direction = random.uniform(20,50)
+            velo_1 = self.velocity.rotate(direction)
+            velo_2 = self.velocity.rotate(-direction)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            child_1 = Asteroid(self.position[0], self.position[1], new_radius)
+            child_2 = Asteroid(self.position[0], self.position[1], new_radius)
+            child_1.velocity = velo_1 * 1.2
+            child_2.velocity = velo_2 * 1.2
+        else: return
